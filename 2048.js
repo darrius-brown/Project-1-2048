11013172b369761111fc0ab606f0ba083664f72c
darriub15 <darriusabrown@gmail.com>
2022-03-18 23:17:54 -0700

    Updated Presentation

--- 2048.js
+++ 2048.js
@@ -69,28 +69,28 @@ function pressedKey(event) {
 startocc = occupied
 merged = []
     if(event.code === `KeyD` || event.code === `ArrowRight`){
-        rightMove(4)
-        rightMove(8)
-        rightMove(12)
-        rightMove(16)
+        rightMove(3)
+        rightMove(7)
+        rightMove(11)
+        rightMove(15)
     }
     if(event.code === `KeyA` || event.code === `ArrowLeft` ){
-        leftMove(-1)
-        leftMove(3)
-        leftMove(7)
-        leftMove(11)
+        leftMove(0)
+        leftMove(4)
+        leftMove(8)
+        leftMove(12)
     }
     if(event.code === `KeyW` || event.code === `ArrowUp`){
-        upMove(-1)
         upMove(0)
         upMove(1)
         upMove(2)
+        upMove(3)
     }
     if(event.code === `KeyS` || event.code === `ArrowDown`){
+        downMove(12)
         downMove(13)
         downMove(14)
         downMove(15)
-        downMove(16)
     }
     endocc = occupied
     if(startocc !== endocc){
@@ -105,267 +105,257 @@ merged = []
     
 }
 function rightMove(lastColumn) {
-    let right2 = document.getElementById(lastColumn - 2)
-    let right1 = document.getElementById(lastColumn - 3)
-    let right0 = document.getElementById(lastColumn - 4)
+    let right2 = document.getElementById(lastColumn - 1)
+    let right1 = document.getElementById(lastColumn - 2)
+    let right0 = document.getElementById(lastColumn - 3)
 
-        if(occupied.includes(lastColumn - 2)){ 
-            if(occupied.includes(lastColumn - 1)) {
-                let mergeRight1 = document.getElementById(lastColumn - 1)
+        if(occupied.includes(lastColumn - 1)){ 
+            if(occupied.includes(lastColumn)) {
+                let mergeRight1 = document.getElementById(lastColumn)
                 if(right2.innerHTML === mergeRight1.innerHTML && merged.includes(mergeRight1.id) === false)
                     merge(right2, mergeRight1) 
                     fixOccupied(right2)
                 } else {
-                    moveSquare(lastColumn - 1, right2)
+                    moveSquare(lastColumn, right2)
                     fixOccupied(right2)
                 }
         }
-        if(occupied.includes(lastColumn - 3)){
-            if(occupied.includes(lastColumn - 2)) {
-                let mergeRight2 = document.getElementById(lastColumn - 2)
+        if(occupied.includes(lastColumn - 2)){
+            if(occupied.includes(lastColumn - 1)) {
+                let mergeRight2 = document.getElementById(lastColumn - 1)
                 if(right1.innerHTML === mergeRight2.innerHTML && merged.includes(mergeRight2.id) === false)
                 merge(right1, mergeRight2)
                 fixOccupied(right1)
         } else {
-            moveSquare(lastColumn - 2, right1)
+            moveSquare(lastColumn - 1, right1)
             fixOccupied(right1)
-            if(occupied.includes(lastColumn - 1)) {
-                let mergeRight3 = document.getElementById(lastColumn - 1)
+            if(occupied.includes(lastColumn)) {
+                let mergeRight3 = document.getElementById(lastColumn)
                 if(right1.innerHTML === mergeRight3.innerHTML && merged.includes(mergeRight3.id) === false)
                 merge(right1, mergeRight3)
                 fixOccupied(right1)
             } else {
-                moveSquare(lastColumn - 1, right1)
+                moveSquare(lastColumn, right1)
                 fixOccupied(right1)
             }
         }
         }
 
-        if(occupied.includes(lastColumn - 4)){
-            if(occupied.includes(lastColumn - 3)) {
-                let mergeRight4 = document.getElementById(lastColumn - 3)
+        if(occupied.includes(lastColumn - 3)){
+            if(occupied.includes(lastColumn - 2)) {
+                let mergeRight4 = document.getElementById(lastColumn - 2)
                 if(right0.innerHTML === mergeRight4.innerHTML && merged.includes(mergeRight4.id) === false)
                 merge(right0, mergeRight4)
                 fixOccupied(right0)
         } else {
-            moveSquare(lastColumn - 3, right0)
+            moveSquare(lastColumn - 2, right0)
             fixOccupied(right0)
-            if(occupied.includes(lastColumn - 2)) {
-                let mergeRight5 = document.getElementById(lastColumn - 2)
+            if(occupied.includes(lastColumn - 1)) {
+                let mergeRight5 = document.getElementById(lastColumn - 1)
                 if(right0.innerHTML === mergeRight5.innerHTML && merged.includes(mergeRight5.id) === false)
                 merge(right0, mergeRight5)
                 fixOccupied(right0)
         } else{
-            moveSquare(lastColumn - 2, right0)
+            moveSquare(lastColumn - 1, right0)
             fixOccupied(right0)
-            if(occupied.includes(lastColumn - 1)) {
-                let mergeRight6 = document.getElementById(lastColumn - 1)
+            if(occupied.includes(lastColumn)) {
+                let mergeRight6 = document.getElementById(lastColumn)
                 if(right0.innerHTML === mergeRight6.innerHTML && merged.includes(mergeRight6.id) === false)
                 merge(right0, mergeRight6)
                 fixOccupied(right0)
         } else{
-            moveSquare(lastColumn - 1, right0)
+            moveSquare(lastColumn, right0)
             fixOccupied(right0)
         }           
 }}}}
 function leftMove(lastColumn) {
-    let left2 = document.getElementById(lastColumn + 2)
-    let left1 = document.getElementById(lastColumn + 3)
-    let left0 = document.getElementById(lastColumn + 4)
+    let left2 = document.getElementById(lastColumn + 1)
+    let left1 = document.getElementById(lastColumn + 2)
+    let left0 = document.getElementById(lastColumn + 3)
     if(occupied.includes(lastColumn + 1)){
-    }
-    if(occupied.includes(lastColumn + 2)){
-        if(occupied.includes(lastColumn + 1)) {
-            let mergeLeft1 = document.getElementById(lastColumn + 1)
+        if(occupied.includes(lastColumn)) {
+            let mergeLeft1 = document.getElementById(lastColumn)
             if(left2.innerHTML === mergeLeft1.innerHTML && merged.includes(mergeLeft1.id) === false)
                 merge(left2, mergeLeft1) 
                 fixOccupied(left2)
         } else {
-            moveSquare(lastColumn + 1, left2)
+            moveSquare(lastColumn, left2)
             fixOccupied(left2)
         }
     }
 
-    if(occupied.includes(lastColumn + 3)){
-        if(occupied.includes(lastColumn + 2)) {
-            let mergeLeft2 = document.getElementById(lastColumn + 2)
+    if(occupied.includes(lastColumn + 2)){
+        if(occupied.includes(lastColumn + 1)) {
+            let mergeLeft2 = document.getElementById(lastColumn + 1)
             if(left1.innerHTML === mergeLeft2.innerHTML && merged.includes(mergeLeft2.id) === false)
                 merge(left1, mergeLeft2) 
                 fixOccupied(left1)
     } else {
-        moveSquare(lastColumn + 2, left1)
+        moveSquare(lastColumn + 1, left1)
         fixOccupied(left1)
-        if(occupied.includes(lastColumn + 1)) {
-            let mergeLeft3 = document.getElementById(lastColumn + 1)
+        if(occupied.includes(lastColumn)) {
+            let mergeLeft3 = document.getElementById(lastColumn)
             if(left1.innerHTML === mergeLeft3.innerHTML && merged.includes(mergeLeft3.id) === false)
                 merge(left1, mergeLeft3) 
                 fixOccupied(left1)
         } else {
-            moveSquare(lastColumn + 1, left1)
+            moveSquare(lastColumn, left1)
             fixOccupied(left1)
         }
     }
     }
 
-    if(occupied.includes(lastColumn + 4)){
-        if(occupied.includes(lastColumn + 3)) {
-            let mergeLeft4 = document.getElementById(lastColumn + 3)
+    if(occupied.includes(lastColumn + 3)){
+        if(occupied.includes(lastColumn + 2)) {
+            let mergeLeft4 = document.getElementById(lastColumn + 2)
             if(left0.innerHTML === mergeLeft4.innerHTML && merged.includes(mergeLeft4.id) === false)
                 merge(left0, mergeLeft4) 
                 fixOccupied(left0)
     } else {
-        moveSquare(lastColumn + 3, left0)
+        moveSquare(lastColumn + 2, left0)
         fixOccupied(left0)
-        if(occupied.includes(lastColumn + 2)) {
-            let mergeLeft5 = document.getElementById(lastColumn + 2)
+        if(occupied.includes(lastColumn + 1)) {
+            let mergeLeft5 = document.getElementById(lastColumn + 1)
             if(left0.innerHTML === mergeLeft5.innerHTML && merged.includes(mergeLeft5.id) === false)
                 merge(left0, mergeLeft5) 
                 fixOccupied(left0)
     } else{
-        moveSquare(lastColumn + 2, left0)
+        moveSquare(lastColumn + 1, left0)
         fixOccupied(left0)
-        if(occupied.includes(lastColumn + 1)) {
-            let mergeLeft6 = document.getElementById(lastColumn + 1)
+        if(occupied.includes(lastColumn)) {
+            let mergeLeft6 = document.getElementById(lastColumn)
             if(left0.innerHTML === mergeLeft6.innerHTML && merged.includes(mergeLeft6.id) === false)
                 merge(left0, mergeLeft6) 
                 fixOccupied(left0)
     } else{
-        moveSquare(lastColumn + 1, left0)
+        moveSquare(lastColumn, left0)
         fixOccupied(left0)
     }           
 }}}}
 function upMove(lastColumn) {
-    let up2 = document.getElementById(lastColumn + 5)
-    let up1 = document.getElementById(lastColumn + 9)
-    let up0 = document.getElementById(lastColumn + 13)
-    if(occupied.includes(lastColumn + 1)){
-    }
-
-    if(occupied.includes(lastColumn + 5)){
-        if(occupied.includes(lastColumn + 1)) {
-            let mergeUp1 = document.getElementById(lastColumn + 1)
+    let up2 = document.getElementById(lastColumn + 4)
+    let up1 = document.getElementById(lastColumn + 8)
+    let up0 = document.getElementById(lastColumn + 12)
+    if(occupied.includes(lastColumn + 4)){
+        if(occupied.includes(lastColumn)) {
+            let mergeUp1 = document.getElementById(lastColumn)
             if(up2.innerHTML === mergeUp1.innerHTML && merged.includes(mergeUp1.id) === false)
                 merge(up2, mergeUp1) 
                 fixOccupied(up2)
             } else {
-                moveSquare(lastColumn + 1, up2)
+                moveSquare(lastColumn, up2)
                 fixOccupied(up2)
             }
     }
 
-    if(occupied.includes(lastColumn + 9)){
-        if(occupied.includes(lastColumn + 5)) {
-            let mergeUp2 = document.getElementById(lastColumn + 5)
+    if(occupied.includes(lastColumn + 8)){
+        if(occupied.includes(lastColumn + 4)) {
+            let mergeUp2 = document.getElementById(lastColumn + 4)
             if(up1.innerHTML === mergeUp2.innerHTML && merged.includes(mergeUp2.id) === false)
                 merge(up1, mergeUp2) 
                 fixOccupied(up1)
     } else {
-        moveSquare(lastColumn + 5, up1)
+        moveSquare(lastColumn + 4, up1)
         fixOccupied(up1)
-        if(occupied.includes(lastColumn + 1)) {
-            let mergeUp3 = document.getElementById(lastColumn + 1)
+        if(occupied.includes(lastColumn)) {
+            let mergeUp3 = document.getElementById(lastColumn)
             if(up1.innerHTML === mergeUp3.innerHTML && merged.includes(mergeUp3.id) === false)
                 merge(up1, mergeUp3) 
                 fixOccupied(up1)
         } else {
-            moveSquare(lastColumn + 1, up1)
+            moveSquare(lastColumn, up1)
             fixOccupied(up1)
         }
     }
     }
 
-    if(occupied.includes(lastColumn + 13)){
-        if(occupied.includes(lastColumn + 9)){
-            let mergeUp4 = document.getElementById(lastColumn + 9)
+    if(occupied.includes(lastColumn + 12)){
+        if(occupied.includes(lastColumn + 8)){
+            let mergeUp4 = document.getElementById(lastColumn + 8)
             if(up0.innerHTML === mergeUp4.innerHTML && merged.includes(mergeUp4.id) === false)
                 merge(up0, mergeUp4) 
                 fixOccupied(up0)
     } else {
-        moveSquare(lastColumn + 9, up0)
+        moveSquare(lastColumn + 8, up0)
         fixOccupied(up0)
-        if(occupied.includes(lastColumn + 5)) {
-            let mergeUp5 = document.getElementById(lastColumn + 5)
+        if(occupied.includes(lastColumn + 4)) {
+            let mergeUp5 = document.getElementById(lastColumn + 4)
             if(up0.innerHTML === mergeUp5.innerHTML && merged.includes(mergeUp5.id) === false)
                 merge(up0, mergeUp5) 
                 fixOccupied(up0)
     } else{
-        moveSquare(lastColumn + 5, up0)
+        moveSquare(lastColumn + 4, up0)
         fixOccupied(up0)
-        if(occupied.includes(lastColumn + 1)) {
-            let mergeUp6 = document.getElementById(lastColumn + 1)
+        if(occupied.includes(lastColumn)) {
+            let mergeUp6 = document.getElementById(lastColumn)
             if(up0.innerHTML === mergeUp6.innerHTML && merged.includes(mergeUp6.id) === false)
                 merge(up0, mergeUp6) 
                 fixOccupied(up0)
-            //check value of 3
-            //merge or dont merge
     } else{
-        moveSquare(lastColumn + 1, up0)
+        moveSquare(lastColumn, up0)
         fixOccupied(up0)
     }           
 }}}}
 function downMove(lastColumn) {
-    let down2 = document.getElementById(lastColumn - 5)
-    let down1 = document.getElementById(lastColumn - 9)
-    let down0 = document.getElementById(lastColumn - 13)
-    if(occupied.includes(lastColumn - 1)){
-    }
-
-    if(occupied.includes(lastColumn - 5)){
-        if(occupied.includes(lastColumn - 1)) {
-            let mergeDown1 = document.getElementById(lastColumn - 1)
+    let down2 = document.getElementById(lastColumn - 4)
+    let down1 = document.getElementById(lastColumn - 8)
+    let down0 = document.getElementById(lastColumn - 12)
+    if(occupied.includes(lastColumn - 4)){
+        if(occupied.includes(lastColumn)) {
+            let mergeDown1 = document.getElementById(lastColumn)
             if(down2.innerHTML === mergeDown1.innerHTML && merged.includes(mergeDown1.id) === false)
                 merge(down2, mergeDown1) 
                 fixOccupied(down2)
         } else {
-            moveSquare(lastColumn - 1, down2)
+            moveSquare(lastColumn, down2)
             fixOccupied(down2)
         }
     }
 
-    if(occupied.includes(lastColumn - 9)){
-        if(occupied.includes(lastColumn - 5)) {
-            let mergeDown2 = document.getElementById(lastColumn - 5)
+    if(occupied.includes(lastColumn - 8)){
+        if(occupied.includes(lastColumn - 4)) {
+            let mergeDown2 = document.getElementById(lastColumn - 4)
             if(down1.innerHTML === mergeDown2.innerHTML && merged.includes(mergeDown2.id) === false)
                 merge(down1, mergeDown2) 
                 fixOccupied(down1)
     } else {
-        moveSquare(lastColumn - 5, down1)
+        moveSquare(lastColumn - 4, down1)
         fixOccupied(down1)
-        if(occupied.includes(lastColumn - 1)) {
-            let mergeDown3 = document.getElementById(lastColumn - 1)
+        if(occupied.includes(lastColumn)) {
+            let mergeDown3 = document.getElementById(lastColumn)
             if(down1.innerHTML === mergeDown3.innerHTML && merged.includes(mergeDown3.id) === false)
                 merge(down1, mergeDown3) 
                 fixOccupied(down1)
         } else {
-            moveSquare(lastColumn - 1, down1)
+            moveSquare(lastColumn, down1)
             fixOccupied(down1)
         }
     }
     }
-    if(occupied.includes(lastColumn - 13)){
-        if(occupied.includes(lastColumn - 9)) {
-            let mergeDown4 = document.getElementById(lastColumn - 9)
+    if(occupied.includes(lastColumn - 12)){
+        if(occupied.includes(lastColumn - 8)) {
+            let mergeDown4 = document.getElementById(lastColumn - 8)
             if(down0.innerHTML === mergeDown4.innerHTML && merged.includes(mergeDown4.id) === false)
                 merge(down0, mergeDown4) 
                 fixOccupied(down0)
     } else {
-        moveSquare(lastColumn - 9, down0)
+        moveSquare(lastColumn - 8, down0)
         fixOccupied(down0)
-        if(occupied.includes(lastColumn - 5)) {
-            let mergeDown5 = document.getElementById(lastColumn - 5)
+        if(occupied.includes(lastColumn - 4)) {
+            let mergeDown5 = document.getElementById(lastColumn - 4)
             if(down0.innerHTML === mergeDown5.innerHTML && merged.includes(mergeDown5.id) === false)
                 merge(down0, mergeDown5) 
                 fixOccupied(down0)
     } else{
-        moveSquare(lastColumn - 5, down0)
+        moveSquare(lastColumn - 4, down0)
         fixOccupied(down0)
-        if(occupied.includes(lastColumn - 1)) {
-            let mergeDown6 = document.getElementById(lastColumn - 1)
+        if(occupied.includes(lastColumn)) {
+            let mergeDown6 = document.getElementById(lastColumn)
             if(down0.innerHTML === mergeDown6.innerHTML && merged.includes(mergeDown6.id) === false)
                 merge(down0, mergeDown6) 
                 fixOccupied(down0)
     } else{
-        moveSquare(lastColumn - 1, down0)
+        moveSquare(lastColumn, down0)
         fixOccupied(down0)
     }           
 }}}}
